a12f15852a348f30cd66e8f7cfb9e0d6ae961cb4
Esoh124 <1111siso@naver.com>
2022-04-29 11:54:59 +0900

    로그인, 회원가입 창 추가, 연결

---
+++ MobileProgramming2/app/src/main/java/sw2022/gachon/mobileprogramming/Login.java
@@ -0,0 +1,35 @@
+package sw2022.gachon.mobileprogramming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button loginbutton= findViewById(R.id.loginbtn);
+        Button signupbutton=findViewById(R.id.signupbtn);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- MobileProgramming2/app/src/main/java/sw2022/gachon/mobileprogramming/SignUp.java
+++ MobileProgramming2/app/src/main/java/sw2022/gachon/mobileprogramming/SignUp.java
@@ -1,4 +1,5 @@
 package sw2022.gachon.mobileprogramming;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -6,18 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class friend_add extends AppCompatActivity {
+
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friend_add);
-
-        Button button = findViewById(R.id.add_newfriend);
+        setContentView(R.layout.activity_sign_up);
+        Button button = findViewById(R.id.accountBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent= new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
             }
         });
