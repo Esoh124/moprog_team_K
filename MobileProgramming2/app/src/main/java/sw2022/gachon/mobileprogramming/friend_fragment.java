1b53cc503cf17d2d2b6d635c8c7d28d250ff41b1
Esoh124 <1111siso@naver.com>
2022-04-29 14:03:43 +0900

    버튼 연결 완료, 이미지 추가

--- MobileProgramming2/app/src/main/java/sw2022/gachon/mobileprogramming/friend_fragment.java
+++ MobileProgramming2/app/src/main/java/sw2022/gachon/mobileprogramming/friend_fragment.java
@@ -11,15 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link friend_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class friend_fragment extends Fragment {
+public class friend_fragment extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -32,47 +25,54 @@ public class friend_fragment extends Fragment {
 
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment friend_fragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static friend_fragment newInstance(String param1, String param2) {
         friend_fragment fragment = new friend_fragment();
-
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Activity context = null;
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        /*Button button = getView().findViewById(R.id.friend_add);
+
+        /**Button button = getView().findViewById(R.id.friend_add);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context.getApplicationContext(), friend_add.class);
+                Intent intent = new Intent(getView(), friend_add.class);
                 startActivity(intent);
             }
-        });*/
+        });**/
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend_fragment, container, false);
+        View v = inflater.inflate(R.layout.fragment_friend_fragment, container, false);
+        Button button=(Button)v.findViewById(R.id.friend_add);
+        button.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.friend_add:
+                Intent intent = new Intent(view.getContext(), friend_add.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
